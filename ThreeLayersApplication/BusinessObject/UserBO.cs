caa919917dfa2bccfc5b1768de3943c02bce5315
Theja_G <150552332+GANDHAPUDITHEJA@users.noreply.github.com>
2025-01-17 11:39:12 +0530

    ThreeLayerApplication

---
+++ ThreeLayersApplication/BusinessObject/UserBO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace BusinessObject
+{
+    public class UserBO
+    {
+        public int UserId {  get; set; }
+        [Display(Name ="User Name")]
+        [Required(ErrorMessage ="User name is required.")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "User Address is required.")]
+        [Display(Name = "User Address")]
+        public string UserAddress { get; set; }
+        [Required(ErrorMessage = "User Email is required.")]
+        [Display(Name = "User Email")]
+        [EmailAddress(ErrorMessage ="please enter valid email address.")]
+        public string UserEmail { get; set; }
+        [Required(ErrorMessage = "User Phone is required.")]
+        [Display(Name = "User Phone")]
+        public string UserPhone { get; set; }
+    }
+}
---
+++ ThreeLayersApplication/DataAccess1/UserDAL.cs
@@ -0,0 +1,50 @@
+﻿using BusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess1
+{
+
+    public class UserDAL
+    {
+        private UserDBEntities objUserDbEntities;
+        public UserDAL()
+        {
+            objUserDbEntities = new UserDBEntities();
+        }
+        public CustomBO AddUser(UserBO objUserBo)
+        {
+            CustomBO objCustomBo = new CustomBO();
+            User objUser = new User()
+            {
+                UserAddress = objUserBo.UserAddress,
+                UserEmail = objUserBo.UserEmail,
+                UserMobile = objUserBo.UserPhone,
+                UserName = objUserBo.UserName
+            };
+            objUserDbEntities.Users.Add(objUser);
+            int returnValue = GetReturnValue();
+            if (returnValue > 0)
+            {
+                objCustomBo.CustomMessage = "Data Successfully Added.";
+                objCustomBo.CustomMessageNumber = returnValue;
+
+            }
+            else
+            {
+                objCustomBo.CustomMessage = "There is some problem to add User.";
+                objCustomBo.CustomMessageNumber = returnValue;
+
+            }
+            return objCustomBo;
+
+            int GetReturnValue()
+            {
+                return objUserDbEntities.SaveChanges();
+            }
+        }
+    }
+}
