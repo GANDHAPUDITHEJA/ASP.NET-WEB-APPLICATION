00d5cf4d9ebc772beb371c481ddf5d0013e61f7b
Theja_G <150552332+GANDHAPUDITHEJA@users.noreply.github.com>
2025-01-27 20:41:29 +0530

    Task 5

---
+++ Task_5_new/Default.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Task_5_new._Default" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div style="padding:15">
+        <div align="center" style="font-size:30px">User Info Report</div>
+        <div align="center">
+            <asp:TextBox ID="TextBoxInput" runat="server" Width="200px" Placeholder="Enter User Filter"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Load Report" BackColor="#66CCFF" Font-Size="Larger" ForeColor="#660033" Height="70px" OnClick="Button1_Click" Width="234px" />
+            <br />
+            <br />
+            <rsweb:ReportViewer ID="ReportViewer1" runat="server" style="margin-left: 0px" Width="1290px"></rsweb:ReportViewer>
+        </div>
+    </div>
+</asp:Content>
---
+++ Task_5_new/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+
+namespace Task_5_new
+{
+    public partial class _Default : Page
+    {
+        SqlConnection con = new SqlConnection(@"Server=BOOK-IF1O364QE1\SQLEXPRESS01;Database=UserDB;Integrated Security=True;TrustServerCertificate=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportViewer1.LocalReport.Refresh();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string userInput = TextBoxInput.Text.Trim();
+            string query = "SELECT * FROM UserInfo WHERE UserId LIKE @Filter";
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Filter", $"%{userInput}%");
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                ReportViewer1.LocalReport.DataSources.Clear();
+                ReportDataSource rds = new ReportDataSource("DataSet1", dt);
+                ReportViewer1.LocalReport.ReportPath = Server.MapPath("UserInfoReport.rdlc");
+                ReportViewer1.LocalReport.DataSources.Add(rds);
+                ReportViewer1.LocalReport.Refresh();
+            }
+            catch (Exception ex)
+            {
+                Response.Write($"<script>alert('Error: {ex.Message}');</script>");
+            }
+        }
+    }
+}
